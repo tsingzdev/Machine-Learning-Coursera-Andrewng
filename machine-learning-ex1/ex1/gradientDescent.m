e7ba5db555adfcc8d1471c4509ae93df5cc6e385
tsingzdev <huiya.zhu@gmail.com>
2017-09-03 02:32:30 +0800

    Week 2: linear regression, update gradientDescent.m

--- machine-learning-ex1/ex1/gradientDescent.m
+++ machine-learning-ex1/ex1/gradientDescent.m
@@ -7,6 +7,7 @@ function [theta, J_history] = gradientDescent(X, y, theta, alpha, num_iters)
 m = length(y); % number of training examples
 J_history = zeros(num_iters, 1);
 
+
 for iter = 1:num_iters
 
     % ====================== YOUR CODE HERE ======================
@@ -16,11 +17,9 @@ for iter = 1:num_iters
     % Hint: While debugging, it can be useful to print out the values
     %       of the cost function (computeCost) and gradient here.
     %
-
-
-
-
-
+    delta =  (1 /m)*  X' * (X*theta - y);
+    theta = theta - alpha * delta;
+    
 
 
     % ============================================================
@@ -29,5 +28,5 @@ for iter = 1:num_iters
     J_history(iter) = computeCost(X, y, theta);
 
 end
-
+J_history;
 end
