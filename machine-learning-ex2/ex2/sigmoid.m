be3834f06551c944ba7d84599fc4d98c038c5c88
tsingzdev <huiya.zhu@gmail.com>
2017-09-27 23:05:10 +0800

    Finished ex2/sigmoid.m

--- machine-learning-ex2/ex2/sigmoid.m
+++ machine-learning-ex2/ex2/sigmoid.m
@@ -9,9 +9,8 @@ g = zeros(size(z));
 % Instructions: Compute the sigmoid of each value of z (z can be a matrix,
 %               vector or scalar).
 
-
-
-
+%g = (e.^z)./(e.^z + 1);
+g = 1./(1+ 1./e.^z);
 
 % =============================================================
 
