0056f21657f16fdb9122d4bbc5a6e68f209dc296
tsingzdev <huiya.zhu@gmail.com>
2018-06-19 17:46:14 +0800

    finished week4: Regularized Logistic Regression

--- machine-learning-ex3/ex3/lrCostFunction.m
+++ machine-learning-ex3/ex3/lrCostFunction.m
@@ -36,11 +36,31 @@ grad = zeros(size(theta));
 %           grad = grad + YOUR_CODE_HERE (using the temp variable)
 %
 
+%J = ( (1 / m)*sum(-y'*log(sigmoid(X*theta)) - (1-y)'*log(1-sigmoid(X*theta))) ) + (lambda/(2*m)) * sum(theta(2:length(theta)).*theta(2:length(theta)));
 
+%grad = (1/m) * sum(X .* repmat((sigmoid(X*theta) -y), 1, size(X,2)) );
 
+%grad(:,2:length(grad)) = grad(:,length(grad)) + (lambda/m) * theta(2:length(theta))';
 
+predictions =  sigmoid(X*theta);
 
+leftPart = -y' * log(predictions); % or sum( (-y .* log(predictions))
 
+rightPart = (1 - y') * log(1 - predictions); % or 
+
+thetaZero = theta;
+
+thetaZero(1) = 0;
+
+lambaCostPart = (lambda / (2 * m)) * sum(thetaZero .^ 2);
+
+lambdaGradPart = lambda / m * thetaZero;
+
+J = (1 / m) * (leftPart - rightPart) + lambaCostPart;
+
+grad = ((1/m) * (X' * (predictions - y)));% (unregularized)
+
+grad = grad + lambdaGradPart;
 
 
 
